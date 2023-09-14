540fd842b63febb563cb74cd71f666ebd951f4aa
Kenrique <ngwakneri2016@gmail.com>
2023-09-14 22:40:23 +0100

    adds all unique integers in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return(set_1.intersection(set_2))
+def uniq_add(my_list=[]):
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number

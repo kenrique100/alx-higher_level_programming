f8ffb4018d9a4c056ed3cbb961805081692e4021
Kenrique <ngwakneri2016@gmail.com>
2023-10-04 11:44:42 +0100

    initial commit

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-void print_python_int(PyObject *p):
-    return (0)
-print(void print_python_int(PyObject *p))
+def magic_string():
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.count)])

40c9bae0ae818726602a3fa2a905b1098031c931
Kenrique <ngwakenri2016@gmail.com>
2023-10-14 22:05:55 +0000

    Read a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+"""function that reads a text file (UTF8) and prints it to stdout"""
+
+
 def read_file(filename=""):
+    """define function that reads text-file"""
     with open(filename, encoding="utf-8") as f:
-        lineNum = 1
-        while(True):
-            line = f.readline()
-            if not line:
-                break
-            print(line, end="")
-            lineNum += 1
+        print(f.read(), end="")

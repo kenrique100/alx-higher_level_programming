ba0874e2c291132ba6f3f142db4be209fa1173df
Kenrique <ngwakneri2016@gmail.com>
2023-10-14 23:30:37 +0100

    updated

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
+"""function that returns description with simple data structure,
+for JSON serialization of an object"""
+
 
 def class_to_json(obj):
-    if isinstance(obj, (list, tuple)):
-        return [class_to_json(item) for item in obj]
-    elif isinstance(obj, dict):
-        return {class_to_json(k): class_to_json(v) for k, v in obj.items()}
-    elif isinstance(obj, str):
-        return obj
-    elif isinstance(obj, int):
-        return obj
-    elif isinstance(obj, bool):
-        return obj
-    else:
-        # For unsupported types, return None
-        return None
+    """define function that returns dictionary description for simple DS"""
+    return obj.__dict__

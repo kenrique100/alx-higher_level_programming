ed2a82692c8e0c975bd0f6e54199ee0f0c8c6b46
Kenrique <ngwakneri2016@gmail.com>
2023-10-14 23:11:30 +0100

    Write a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""function that writes a string to a text file and returns number of
+characters written"""
+
+
 def write_file(filename="", text=""):
-    with open(filename, 'w', encoding="utf-8") as f:
-        total = f.write(text)
-    return total
+    """define function that writes into a string"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)

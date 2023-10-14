25574c6bf1b6624ddfd772b06f533b19ddc7a032
Kenrique <ngwakneri2016@gmail.com>
2023-10-14 23:33:23 +0100

    updated

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,19 +1,28 @@
-#!/usr/bin/pthon3
-"""function that defines a class; Student that defines a student,
-based on task9"""
+#!/usr/bin/python3
+"""Defines a class Student."""
 
 
 class Student:
-    """define a class that represents a studeent"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """initializes variables for new student"""
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """defines dictionary representation for student"""
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}

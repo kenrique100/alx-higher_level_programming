fde08db8475ca6c5b82b04bfde0c3fe4a9c09575
Kenrique <ngwakneri2016@gmail.com>
2023-10-14 23:29:30 +0100

    Load add and save object

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a python list, and saves them to a file"""
-import sys
+"""Module adds all arguments to a Python list and save them to a file."""
 
+
+import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -13,4 +14,4 @@ if __name__ == "__main__":
     except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_items.json")
+    save_to_json_file(items, "add_item.json")

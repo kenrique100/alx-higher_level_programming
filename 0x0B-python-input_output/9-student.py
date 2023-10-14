0d6a718a854b679d097051cc0167b6bb4aeae028
Kenrique <ngwakneri2016@gmail.com>
2023-10-14 23:31:38 +0100

    updated

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""function that define a class Student, that defines a student by:
+    first name, last name and age"""
+
 
 class Student:
+    """define a class"""
+
     def __init__(self, first_name, last_name, age):
+        """initializes variables for student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        return {
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'age': self.age
-        }
+        """define representation of the student"""
+        return self.__dict__

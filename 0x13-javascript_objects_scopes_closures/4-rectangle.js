aa077ce25954bb0d122370b77d08701f3eb783c7
Kenrique <ngwakneri2016@gmail.com>
2023-12-12 15:26:27 +0100

    Task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
+
 /**
  * Check the parameters provided
  */
+
 class Rectangle {
   constructor (w, h) {
     if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
